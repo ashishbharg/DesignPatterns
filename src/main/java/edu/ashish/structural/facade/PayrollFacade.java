f883f8724b32f15e0ad8d5093a21b3b31c54dcd9
Ashish Bhargava <ashishbharg@gmail.com>
2019-01-02 17:45:46 +0530

    Documentation

--- src/main/java/edu/ashish/structural/facade/PayrollFacade.java
+++ src/main/java/edu/ashish/structural/facade/PayrollFacade.java
@@ -3,6 +3,12 @@ package edu.ashish.structural.facade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * This facade avoids the interaction of different sub-systems which could lead to complex sub-system.
+ * Instead it simplifies the subsystem by exposing a public API in facade class which can acts as interactive
+ * layer with other systems. Client is usually provided this facade class through which it can work across different
+ * sub-systems.
+ */
 @Service
 public class PayrollFacade {
 
